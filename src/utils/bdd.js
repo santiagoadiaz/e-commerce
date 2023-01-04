32748d9595cd2d44ca6a2b06beabea61d4c441a2
SantiagoDiaz <106628869+santiagoadiaz@users.noreply.github.com>
2023-01-04 18:39:28 -0300

    Delete src directory

--- src/utils/bdd.js
+++
@@ -1,2 +0,0 @@
-import { cargarBaseDeDatos } from "./firebase"
-cargarBaseDeDatos()
--- src/utils/funcionesUtiles.js
+++
@@ -1,6 +0,0 @@
-export const consultarBDD = async (ruta) => {
-    const response = await fetch(ruta)
-    const productos = await response.json()
-    return productos
-}
-
